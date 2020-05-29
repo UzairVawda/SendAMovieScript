5f6bcc3bb71c5af2eb77e458a56925fc703f6e49
umv23 <umv23@cs.drexel.edu>
2020-05-29 12:54:19 -0400

    Whatsapp working

--- Whatsapp.py
+++ Whatsapp.py
@@ -17,29 +17,21 @@ userChoice = (input("Enter Recipents Username or Groupchat Name:\n> "))
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(userChoice))
 user.click()
 
-print (driver.find_element_by_class_name('_3F6QL _2WovP'))
-messageBox = driver.find_element_by_class_name('_3F6QL _2WovP')
-print (messageBox)
-# print(userChoice)
-# print(user)
-# print(messageBox)
+messageBox = driver.find_element_by_class_name('_1Plpp')
 
-# messageBoxPath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-# messageBox = wait.until(EC.presence_of_element_located(( By.XPATH, messageBoxPath))) 
 
-# file = input('Name of script file:\n> ')
-# messageDelay = float(input('Delay between each message: '))
+file = input('Name of script file:\n> ')
+messageDelay = float(input('Delay between each message: '))
 
-# with open(file, 'r') as script:
-# 	lines = script.readlines()
-# 	for line in lines:
-# 		line = line.replace("'", "")
-# 		try:
-# 			print (lines)
-# 			messageBox[1].send_key(line + Keys.ENTER)
-# 			print('Sending %s' %(line))
-# 			time.sleep(messageDelay)
-# 		except:
-# 			print("Sorry, something went wrong.")
+with open(file, 'r') as script:
+	lines = script.readlines()
+	for line in lines:
+		line = line.replace("'", "")
+		try:
+			messageBox.send_keys(line)
+			driver.find_element_by_class_name('_35EW6').click()
+			time.sleep(messageDelay)
+		except:
+			print("Sorry, something went wrong.")
 
-# script.close()
+script.close()

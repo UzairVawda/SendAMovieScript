f16214aa63de0e54bd150dff04032a62f3335331
umv23 <umv23@cs.drexel.edu>
2020-05-29 14:50:20 -0400

    Cleaning

--- Whatsapp.py
+++ Whatsapp.py
@@ -1,28 +1,38 @@
 import sys
 import time 
 from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
 
-#driver = webdriver.Chrome("LOCATION OF GOOGLE CHROME DRIVER")
+# Click the three dots in the upper right hand corner 
+# Select help > About Google Chrome
+# https://chromedriver.chromium.org/ Download the appropriate version and replace the path
+# driver = webdriver.Chrome("YOUR GOOGLE CHROME DRIVER PATH")
+
 driver = webdriver.Chrome("/Users/UzairVawda/Sites/ScriptSending/driver/chromedriver")
+
+# Load whatsapp in the chrome beowser and wait 
 driver.get("http://web.whatsapp.com")
-wait = WebDriverWait(driver, 600) 
+wait = WebDriverWait(driver, 1000) 
 
-input("Enter anything after scanning QR code in your web browser!")
+# Check if the user scanned the QR Code
+input("Enter anything after scanning QR code in your web browser! (WAIT FOR THE BROWSER TO FULLY LOAD!)")
 
-userChoice = (input("Enter Recipents Username or Groupchat Name:\n> "))
+# Get the input from the users 
+userChoice = (input("Enter Recipents Username or Groupchat Name: (THIS IS CASE AND EMOJI SENSITIVE)\n> "))
+
+# Select the user tile with the user and then click it
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(userChoice))
 user.click()
 
+# Select the message box 
 messageBox = driver.find_element_by_class_name('_1Plpp')
 
-
+# Select file from user and wait time between messages 
 file = input('Name of script file:\n> ')
 messageDelay = float(input('Delay between each message: '))
 
+# Open file as script and for each line fiter the "'" and send them one by one 
+# I had to filt the "'" becasue they were causing the script to quit or crash 
 with open(file, 'r') as script:
 	lines = script.readlines()
 	for line in lines:

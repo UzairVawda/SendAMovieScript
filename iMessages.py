5b14355d802f6b7b4352962cb2b1d0266fe3942e
umv23 <umv23@cs.drexel.edu>
2020-06-05 14:58:25 -0400

    Added song api

--- iMessages.py
+++ iMessages.py
@@ -1,13 +1,35 @@
+# import os for osascript, import sys to exit, import time for delay, import lyricsgenius for song lyrics
 import os
+import sys
+import time 
+import lyricsgenius
 
+# enter recipents phone number, iniitialize genius
 number = input("Enter Recipents Phone Number:\n(Do Not include dashes or parenthesies. Just the 10 digit number):\n> ")
-file = str(input("File Name:\n> "))
+genius = lyricsgenius.Genius("hsirmoWcQW6Xize7OXuF2WjUZcQqHCpwgksh7uLm-xUL_b5hd3oJ33wOq0SmIX3M")
 
-with open(file, 'r') as script:
-	lines = script.readlines()
-	for line in lines:
-		line = line.replace("'", "")
-		os.system("osascript sendMessages.scpt %s '%s'" %(number, line))
+# specify song and message delay
+specificSong = str(input("What song would you like to search for?\n> "))
+messageDelay = int(input("Delay between each messages in seconds:\n> "))
 
-script.close()
+# search genius for the song
+song = genius.search_song(specificSong)
 
+# check if song exisits
+if (song == None):
+	print ("Could not find", specificSong)
+	sys.exit()
+
+# split song by new line
+songLyrics = song.lyrics
+songLyrics = songLyrics.split("\n")
+
+print ("Sending lyrics to " + number + "...")
+
+# send song line by line
+for line in songLyrics:
+	line = line.replace("'", "")
+	os.system("osascript sendMessages.scpt %s '%s'" %(number, line))
+	time.sleep(messageDelay)
+
+print ("Done sending lyrics!")
